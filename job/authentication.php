0622af61d2d4d16523ac4d54209675a67e00d0ae
naveen-kc <naveen@active.agency>
2022-12-06 10:06:19 +0530

    admin file added

---
+++ job/authentication.php
@@ -0,0 +1,23 @@
+<?php      
+    include('connection.php');  
+    $mobile = $_POST['mobile'];  
+    $password = $_POST['pass'];  
+      
+        //to prevent from mysqli injection  
+        $mobile = stripcslashes($mobile);  
+        $password = stripcslashes($password);  
+        $mobile = mysqli_real_escape_string($con, $mobile);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from user_details where mobile = '$mobile' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid mobile or password.</h1>";  
+        }     
+?>  
